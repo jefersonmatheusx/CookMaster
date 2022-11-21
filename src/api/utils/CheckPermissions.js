ff7bc8fbef01270bcdef00aa05284b97e8a21a6a
JEFERSON_MATHEUS <jeferson.matheus1@gmail.com>
2022-11-21 18:51:33 -0300

    fix: change permission to a switch case

--- src/api/utils/CheckPermissions.js
+++ src/api/utils/CheckPermissions.js
@@ -1,9 +1,17 @@
 module.exports = async (recipeModel, id, userIdReq, userRole) => {
-  const { userId } = await recipeModel.getById(id)
-  if (userRole !== 'admin' && userRole == 'user') {
-    if (userId.toString() !== userIdReq) {
-      return false
-    }
+  const user = await recipeModel.getById(id)
+  if (!user) {
+    return false
   }
-  return true
+  switch (userRole) {
+    case 'admin':
+      return true
+    case 'user':
+      if (user.userId.toString() !== userIdReq) {
+        return false
+      }
+      return true
+    default:
+      return false
+  }
 }
